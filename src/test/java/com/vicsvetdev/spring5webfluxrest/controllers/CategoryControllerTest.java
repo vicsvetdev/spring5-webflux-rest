efc287ed16eea6900961d98a177e33b5d40f5918
Victor Svetuskin <vicsvetdev@gmail.com>
2021-04-05 21:35:05 +0300

    Adding Category create method to controller and test for it.

--- src/test/java/com/vicsvetdev/spring5webfluxrest/controllers/CategoryControllerTest.java
+++ src/test/java/com/vicsvetdev/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -6,9 +6,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
 
 public class CategoryControllerTest {
 
@@ -45,5 +48,20 @@ public class CategoryControllerTest {
                 .uri("/api/v1/categories/someId")
                 .exchange()
                 .expectBodyList(Category.class);
+    }
+
+    @Test
+    public void createCategory() {
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().build()));
+
+        Mono<Category> categoryToSaveMono = Mono.just(Category.builder().description("description").build());
+
+        webTestClient.post()
+                .uri("/api/v1/categories")
+                .body(categoryToSaveMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
     }
 }
